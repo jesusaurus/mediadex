9285c34374bfd26e33c2307600ed3109b0b5c2bb
K Jonathan Harker <code@gentlydownthe.net>
2020-06-07 20:03:23 +0000

    Move Item to its own module

--- mediadex/indexer/__init__.py
+++ mediadex/indexer/__init__.py
@@ -23,122 +23,14 @@ from elasticsearch_dsl import connections
 from elasticsearch_dsl import FacetedSearch
 from elasticsearch_dsl import TermsFacet
 
-from mediadex import AudioStream
 from mediadex import Movie
 from mediadex import Song
-from mediadex import TextStream
-from mediadex import VideoStream
 from mediadex.exc import IndexerException
 from mediadex.indexer.movie import MovieIndexer
 from mediadex.indexer.song import SongIndexer
+from mediadex.item import Item
 
 LOG = logging.getLogger('mediadex.indexer')
-
-
-class Item:
-    def __init__(self, data):
-        gen = [t for t in data if t['track_type'] == 'General']
-        if len(gen) > 1:
-            raise IndexerException("More than one General track found")
-        elif len(gen) == 0:
-            raise IndexerException("No General track found")
-        self.general = gen.pop()
-
-        atracks = [t for t in data if t['track_type'] == 'Audio']
-        self.audio_tracks = atracks
-        acount = len(atracks)
-
-        vtracks = [t for t in data if t['track_type'] == 'Video']
-        self.video_tracks = vtracks
-        vcount = len(vtracks)
-
-        ttracks = [t for t in data if t['track_type'] == 'Text']
-        self.text_tracks = ttracks
-        tcount = len(ttracks)
-
-        if vcount > 0 and acount > 0:
-            self.dex_type = 'movie'
-        elif vcount > 0:
-            self.dex_type = 'image'
-        elif acount == 1:
-            self.dex_type = 'song'
-        elif tcount == 1:
-            self.dex_type = 'text'
-        elif acount == 0 and vcount == 0 and tcount == 0:
-            self.dex_type = 'empty'
-        else:
-            self.dex_type = 'unknown'
-
-    def base_track(self, track, stream):
-        if 'duration' in track:
-            try:
-                stream.duration = float(track['duration'])
-            except ValueError as exc:
-                if LOG.isEnabledFor(logging.INFO):
-                    LOG.exception(exc)
-                else:
-                    LOG.warn(str(exc))
-
-        if 'format' in track:
-            stream.codec = track['format']
-        if 'format_profile' in track:
-            stream.codec_profile = track['format_profile']
-        if 'language' in track:
-            stream.language = track['language']
-        if 'internet_media_type' in track:
-            stream.mime_type = track['internet_media_type']
-
-    def astreams(self):
-        for t in self.audio_tracks:
-            stream = AudioStream()
-
-            self.base_track(t, stream)
-
-            if 'channel_s' in t:
-                stream.channels = t['channel_s']
-            if 'bit_rate' in t:
-                stream.bit_rate = t['bit_rate']
-            if 'sampling_rate' in t:
-                stream.sample_rate = t['sampling_rate']
-
-            yield stream
-
-    def tstreams(self):
-        for t in self.text_tracks:
-            stream = TextStream()
-
-            self.base_track(t, stream)
-
-            yield stream
-
-    def vstreams(self):
-        for t in self.video_tracks:
-            stream = VideoStream()
-
-            self.base_track(t, stream)
-
-            if 'bit_rate' in t:
-                stream.bit_rate = t['bit_rate']
-
-            if 'bit_depth' in t:
-                stream.bit_depth = t['bit_depth']
-
-            if 'height' in t and 'width' in t:
-                try:
-                    stream.height = int(t['height'])
-                    stream.width = int(t['width'])
-
-                    stream.resolution = "{0}x{1}".format(
-                            t['height'],
-                            t['width'],
-                    )
-                except ValueError as exc:
-                    if LOG.isEnabledFor(logging.INFO):
-                        LOG.exception(exc)
-                    else:
-                        LOG.warn(str(exc))
-
-            yield stream
 
 
 class FileListFacet(FacetedSearch):
